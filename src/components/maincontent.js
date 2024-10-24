d4991bf8ce7f464596ad793db4e4f3350b5146f3
Drojecki <127322846+Drojecki@users.noreply.github.com>
2024-10-24 17:20:20 +0200

    Home responsivness

--- src/components/maincontent.js
+++ src/components/maincontent.js
@@ -11,11 +11,27 @@ function Maincontent() {
                         <p>I'm a Fullstack Developer with experience in building web applications, specializing in React, JavaScript, and Angular.<br /> I have strong analytical skills, creativity, and the ability to work effectively under pressure.</p>
                     </div>
                     <div className='introductionImg'>
-                    <img className='LogoImgIntroduction' src="/nunu.png" alt="Logo"/>
+                        <img className='LogoImgIntroduction' src="/nunu.png" alt="Logo" />
                     </div>
                 </div>
+                <div className='lineContainer'>
+                    <div className='line left'></div>
+                    <p>TechStack</p>
+                    <div className='line right'></div>
+                </div>
                 <div className='techStack'>
-                    <img className='TechImg' src="/Tech.png" alt="Logo"/>
+                    <img className='TechImg' src="Tech/PHP.png" alt="Logo" />
+                    <img className='TechImg' src="Tech/HTML.png" alt="Logo" />
+                    <img className='TechImg' src="Tech/JS.png" alt="Logo" />
+                    <img className='TechImg' src="Tech/React.webp" alt="Logo" />
+                    <img className='TechImg' src="Tech/Angular.png" alt="Logo" />
+                    <img className='TechImg' src="Tech/NODEJS.png" alt="Logo" />
+                    <img className='TechImg' src="Tech/CSS.png" alt="Logo" />
+                    <img className='TechImg' src="Tech/MYSQL.png" alt="Logo" />
+                </div>
+                <div className='lineContainer'>
+                    <div className='line left'></div>
+                    <div className='line right'></div>
                 </div>
             </div>
         </>
